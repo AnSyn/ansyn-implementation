0f7745ab336f4284852c8875476a4e81cd18408e
YairTawil <yairtawil@gmail.com>
2018-09-13 14:13:40 +0300

    implementation of @ansyn/ansyn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { LoadDefaultCaseAction } from '@ansyn/menu-items';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(store: Store<any>) {
+    store.dispatch(new LoadDefaultCaseAction());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AnsynModule } from '@ansyn/ansyn';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    RouterModule.forRoot([]),
+    AnsynModule
   ],
   providers: [],
   bootstrap: [AppComponent]
