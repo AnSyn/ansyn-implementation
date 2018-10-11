53922e0a30d5f10592123a2244621b002f66e801
YairTawil <yairtawil@gmail.com>
2018-10-11 16:53:07 +0300

    feat: ansyn api (#1)
    * feat: ansyn api
    
    * add AnsynApi on README
    
    * css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AnsynApi } from '@ansyn/ansyn';
+import { LayoutKey, layoutOptions } from '@ansyn/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  layoutKeys: LayoutKey[] = Array.from(layoutOptions.keys());
+
+  constructor(protected ansynApi: AnsynApi) {
+  }
+
+  setLayout(layout: LayoutKey): void {
+    this.ansynApi.changeMapLayout(layout);
+  }
 }
