54149d0f6437ea2f78869b15cf17f7c847c271ae
Danaorlov1005 <33423198+Danaorlov1005@users.noreply.github.com>
2018-11-13 15:49:43 +0200

    Add annotations from api (#3)
    * ram
    
    * added api set features and delete features to show carmel
    
    * (fix): fixed pr requested changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AnsynApi } from '@ansyn/ansyn';
 import { LayoutKey, layoutOptions } from '@ansyn/core';
+import { FeatureCollection, Feature } from 'geojson';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +12,32 @@ import { LayoutKey, layoutOptions } from '@ansyn/core';
 })
 export class AppComponent {
   layoutKeys: LayoutKey[] = Array.from(layoutOptions.keys());
+  featureOptions: FeatureCollection<any>;
 
-  constructor(protected ansynApi: AnsynApi) {
+  constructor(protected ansynApi: AnsynApi,
+    private http: HttpClient) {
+
+    this.getFeatureCollectionFromFile().subscribe(response => {
+    this.featureOptions = response.featureCollection;
+    });
+    this.ansynApi.getActiveCenter$.subscribe((center) => {
+      console.log(center);
+    });
+  }
+
+  setFeatureCollection(): void {
+    this.ansynApi.setAnnotations(this.featureOptions);
+  }
+
+  deleteFeatureCollection(): void {
+    this.ansynApi.deleteAllAnnotations();
   }
 
   setLayout(layout: LayoutKey): void {
     this.ansynApi.changeMapLayout(layout);
+  }
+
+  getFeatureCollectionFromFile(): Observable<any> {
+    return this.http.get('assets/featureCollection.json')
   }
 }
