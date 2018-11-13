88b680d6252aa52dc9b2b0c2d51a2c8125bb1d9f
danaorlov1005 <dandik.o12@gmail.com>
2018-11-13 15:37:54 +0200

    added api set features and delete features to show carmel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
 import { Component } from '@angular/core';
 import { AnsynApi } from '@ansyn/ansyn';
 import { LayoutKey, layoutOptions } from '@ansyn/core';
+import { FeatureCollection, Feature } from 'geojson';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})  
+})
 export class AppComponent {
   layoutKeys: LayoutKey[] = Array.from(layoutOptions.keys());
+  featureOptions: FeatureCollection<any>;
 
-  constructor(protected ansynApi: AnsynApi) {
-    this.ansynApi.getActiveCenter$.subscribe((aaa)=>{
+  constructor(protected ansynApi: AnsynApi,
+    private http: HttpClient) {
+
+    this.getFeatureCollectionFromFile().subscribe(res => {
+      console.log(res);
+    this.featureOptions = res.featureCollection;
+    });
+    this.ansynApi.getActiveCenter$.subscribe((aaa) => {
       console.log(aaa);
     });
   }
 
+  setFeatureCollection(): void {
+    this.ansynApi.setAnnotations(this.featureOptions);
+  }
+
+  deleteFeatureCollection(): void {
+    this.ansynApi.deleteAllAnnotations();
+  }
+
   setLayout(layout: LayoutKey): void {
     this.ansynApi.changeMapLayout(layout);
+  }
+
+  getFeatureCollectionFromFile(): Observable<any> {
+    return this.http.get('assets/featureCollection.json')
   }
 }
