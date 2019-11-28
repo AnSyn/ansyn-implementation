d2c88c195a65f5b9955c4d0c8635c0eb27a8435f
Ram Tobolski <ramtob@users.noreply.github.com>
2019-11-28 15:44:17 +0200

    search point or polygon with radius (#40)
    * Adding <button>s and <input>s to search by point or polygon with additional radius
    
    * Lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import {NgModule} from '@angular/core';
-import {AnsynModule} from '@ansyn/ansyn';
-import {OL_CONFIG, OL_PLUGINS_CONFIG} from '@ansyn/ol';
-import {AppComponent} from './app.component';
-import {CustomMenuComponent} from './menus/custom-menu/custom-menu.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {StoreModule} from '@ngrx/store';
-import {EffectsModule} from '@ngrx/effects';
+import { NgModule } from '@angular/core';
+import { AnsynModule } from '@ansyn/ansyn';
+import { OL_CONFIG, OL_PLUGINS_CONFIG } from '@ansyn/ol';
+import { AppComponent } from './app.component';
+import { CustomMenuComponent } from './menus/custom-menu/custom-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import {EffectsModule} from '@ngrx/effects';
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     AnsynModule.component(),
+    FormsModule,
   ],
   providers: [
     {
